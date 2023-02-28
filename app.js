8597e3c89838af24046e6f3beb71dcf6ee51526c
João <joao.rochap03@gmail.com>
2023-02-28 01:11:42 -0300

    Pausado na aula 68

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const app = require('./config/server');
 
-app.listen('80', () => {
+
+const server = app.listen('80', () => {
     console.log('Servidor online');
+}); 
+
+
+// Parametrizar servidor socket.io
+
+// Versão 2.0.3 de socket.io
+const io = require('socket.io').listen(server); // Na porta 80 tantos requisições HTTP e requisições websockets serão recebidas e interpretadas
+
+// Criar a conexão por websocket
+io.on('connection', (socket) => {
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectou');
+    });
 });
